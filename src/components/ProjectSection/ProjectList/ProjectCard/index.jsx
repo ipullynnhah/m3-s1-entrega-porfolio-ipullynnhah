922993df4955ec6b7c526140c49baf52d944edbf
ipullynnhah <ipullynnhah@gmail.com>
2024-04-10 13:27:08 -0300

    feat: implement link icon component

--- src/components/ProjectSection/ProjectList/ProjectCard/index.jsx
+++ src/components/ProjectSection/ProjectList/ProjectCard/index.jsx
@@ -1,10 +1,15 @@
+import githubIcon from "../../../../assets/github-icon-white.svg";
 import { LinkIcon } from "../../../fragments/LinkIcon/index.jsx";
-import whatsappIcon from "../../../../assets/whatsapp-icon.svg";
 
-export function ProjectCard() {
+export function ProjectCard({ name, link, repo, description }) {
   return (
-    <div>
-      <LinkIcon name={"WhatsApp"} url={"https://wa.me"} icon={whatsappIcon} />
-    </div>
+    <li>
+      <div>
+        <h3>{name}</h3>
+        <LinkIcon name={name} url={repo} icon={githubIcon} />
+      </div>
+      <p>{description}</p>
+      <a href={link}>Saiba mais</a>
+    </li>
   );
 }
--- src/components/ProjectSection/ProjectList/index.jsx
+++ src/components/ProjectSection/ProjectList/index.jsx
@@ -1,11 +1,16 @@
-import { ProjectCard } from "./ProjectCard"; // TODO: need to add #projects to this
+import { ProjectCard } from "./ProjectCard";
+import { projects } from "../../../data/projects.js"; // TODO: need to add #projects to this
 
 // TODO: need to add #projects to this
 export function ProjectList() {
   return (
-    <>
-      <h3>ProjectList</h3>
-      <ProjectCard />
-    </>
+    <ul>
+      <ProjectCard
+        name={projects[0].name}
+        link={projects[0].link}
+        repo={projects[0].repo}
+        description={projects[0].description}
+      />
+    </ul>
   );
 }

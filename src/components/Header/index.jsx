ae9e0c859908dac5a7f111961c9b764e7a45581f
ipullynnhah <ipullynnhah@gmail.com>
2024-04-10 20:24:23 -0300

    feat: style module.css for header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,10 +2,11 @@ import logo from "../../assets/logo.svg";
 import { Link } from "../fragments/Link";
 
 import { Menu } from "./Menu";
+import styles from "./style.module.css";
 
 export function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <img src={logo} alt={"Logo do Portfólio"} />
       <Menu />
       <Link url={"#contact"}>Contato</Link>
--- src/pages/TemplatePage/index.jsx
+++ src/pages/TemplatePage/index.jsx
@@ -1,12 +1,14 @@
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 
+import styles from "./style.module.css";
+
 export function TemplatePage({ children }) {
   return (
-    <>
+    <div className={styles.page}>
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }

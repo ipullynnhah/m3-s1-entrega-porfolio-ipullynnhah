e46c36cf5b100183088dda94cdcf8fbfda418fea
ipullynnhah <ipullynnhah@gmail.com>
2024-04-10 11:31:57 -0300

    feat: implement header component (TODO)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,15 @@
+import logo from "../../assets/logo.svg";
+import { Button } from "../fragments/Button";
+
 import { Menu } from "./Menu";
 
 export function Header() {
   return (
-    <>
-      <h2>Header</h2>
+    <header>
+      <img src={logo} alt={"Logo do Portfólio"} />
       <Menu />
-    </>
+      {/*TODO: need to pass parameters to Button*/}
+      <Button />
+    </header>
   );
 }

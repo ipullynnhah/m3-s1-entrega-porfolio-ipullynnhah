da419493de9cd571cfbd8bbdb3146755d53382d9
ipullynnhah <ipullynnhah@gmail.com>
2024-04-10 13:13:57 -0300

    feat: implement tech list component

--- src/components/TechSection/TechList/index.jsx
+++ src/components/TechSection/TechList/index.jsx
@@ -5,11 +5,13 @@ import { TechCard } from "./TechCard";
 // TODO: need to add #stacks to this
 export function TechList() {
   return (
-    <>
+    <div>
       <h3>TechList</h3>
       <ul>
-        <TechCard name={technologies[0].name} icon={technologies[0].icon} />
+        {technologies.map(({ id, name, icon }) => (
+          <TechCard key={id} name={name} icon={icon} />
+        ))}
       </ul>
-    </>
+    </div>
   );
 }

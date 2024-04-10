39543a2d553024f348dc08341a05822cb96552a7
ipullynnhah <ipullynnhah@gmail.com>
2024-04-10 13:06:25 -0300

    feat: implment about me section

--- src/components/AboutMeSection/index.jsx
+++ src/components/AboutMeSection/index.jsx
@@ -1,4 +1,15 @@
 // TODO: need to add #about-me to this
 export function AboutMeSection() {
-  return <h2>AboutMeSection</h2>;
+  return (
+    <section>
+      <h2>Sobre mim</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ex magna,
+        imperdiet sodales sem quis, sollicitudin lobortis purus. Etiam a ipsum
+        finibus, dictum leo non, ultrices dui. Nunc id felis pharetra, vehicula
+        enim in, suscipit nisi. Mauris eget sapien a velit facilisis ullamcorper
+        feugiat nec orci. Duis ac iaculis turpis, at feugiat orci.
+      </p>
+    </section>
+  );
 }
